9d668c5aeb94f52b3c9612420544d9979dbb741e
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2024-03-25 12:20:37 +0530

    Perform CRUD operation on the events

--- evently-server/controllers/eventController.js
+++ evently-server/controllers/eventController.js
@@ -7,14 +7,25 @@ const Event = require("../models/eventModel");
 // @route   GET /api/events
 // @access  Public
 const getEvents = asyncHandler(async (req, res) => {
-  res.json("getEvents");
+  // Fetch all events from the database
+  const events = await Event.findAll();
+
+  // Respond with the events
+  res.json(events);
 });
 
 // @desc    Fetch single event
 // @route   GET /api/events/:id
 // @access  Public
 const getEvent = asyncHandler(async (req, res) => {
-  res.json("getEvent");
+  // get the event id from the request parameters
+  const eventId = req.params.id;
+
+  // Fetch the event from the database
+  const event = await Event.findByPk(eventId);
+
+  // Respond with the event
+  res.json(event);
 });
 
 // @desc    Create an event
@@ -49,14 +60,53 @@ const createEvent = asyncHandler(async (req, res) => {
 // @route   PUT /api/events/:id
 // @access  Private/Admin
 const updateEvent = asyncHandler(async (req, res) => {
-  res.json("updateEvent");
+  // get the event id from the request parameters
+  const eventId = req.params.id;
+
+  // Extract event details from request body or wherever they come from
+  const eventData = {
+    title: req.body.title,
+    description: req.body.description,
+    date: req.body.date,
+    time: req.body.time,
+    location: req.body.location,
+    image: req.body.image,
+  };
+
+  // Update the event in the database
+  const updatedEvent = await Event.update(eventData, {
+    where: {
+      id: eventId,
+    },
+  });
+
+  // Respond with the updated event
+  res.status(200).json({
+    updated: updatedEvent,
+    message: "Event updated successfully",
+    data: eventData,
+  });
 });
 
 // @desc    Delete an event
 // @route   DELETE /api/events/:id
 // @access  Private/Admin
 const deleteEvent = asyncHandler(async (req, res) => {
-  res.json("deleteEvent");
+  // get id from the request parameters
+  const eventId = req.params.id;
+
+  // Delete the event from the database
+  const deletedEvent = await Event.destroy({
+    where: {
+      id: eventId,
+    },
+  });
+
+  // Respond with the deleted event
+  res.status(200).json({
+    deleted: deletedEvent,
+    message: `Event ${eventId} deleted successfully`,
+  });
 });
 
 module.exports = {
