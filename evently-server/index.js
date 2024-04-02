1066cdc54fbe924f70bae5e1e5c33c53a5e235e9
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2024-04-02 12:13:53 +0530

    Error handler added

--- evently-server/index.js
+++ evently-server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const app = express();
 const dotenv = require("dotenv").config();
 const { dbConnection } = require("./config/dbConnection");
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/v1/events", require("./routes/eventRoutes"));
 app.use("/api/v1/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
